f61610e0b55bd41c4f7ae1afde5e9e425762c04d
AYD <51245362+AYD61@users.noreply.github.com>
2019-11-27 15:29:16 +0800

    Add files via upload

---
+++ js/ts.js
@@ -0,0 +1,15 @@
+function ts(i) {
+	var ts = document.createElement("span");
+	var body = document.getElementsByTagName("body");
+	ts.classList.add("ts")
+	ts.innerHTML = i;
+	document.body.appendChild(ts);
+	$(".ts").hide();
+	$(".ts").fadeIn(500);
+	setTimeout(function() {
+		$(".ts").fadeOut(500);
+		setTimeout(function() {
+			document.body.removeChild(ts);
+		},500)
+	}, 1500);
+}
